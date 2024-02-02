981bd3e0affe454f09c11ba2a9e43d928a547f8d
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-02-02 16:08:58 +0100

    alternate for n < 5

--- scripts/helper_function.js
+++ scripts/helper_function.js
@@ -2,6 +2,16 @@ function get_random_stimulus(){
     const stim = Math.random() > 0.5 ? possible_stimuli[0] : possible_stimuli[1];
     return `<p class = "rat-stim">${stim}</p>`;
 }
+function get_alternate_stimulus(){
+    let previous_stim = null;
+    if (jsPsych.data.get().filter([{type: 'trial'}]).last(1).values()[0] == null | trial_num === 1){
+          previous_stim = possible_stimuli[0];
+    } else {
+          previous_stim = jsPsych.data.get().filter([{type: 'trial'}]).last(1).values()[0].stimulus;
+    }  
+    alternate_stim = possible_stimuli.find(item => item !== extract_stim_from_html(previous_stim));
+    return `<p class = "rat-stim">${alternate_stim}</p>`
+}
 
 function extract_stim_from_html(string){
     var match = /<p class = "rat-stim">(.+)<\/p>/i.exec(string);
