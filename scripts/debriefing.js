210fa6b0057db49191fe3a82fa1862362026d1ee
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-04-30 11:31:03 +0200

    fixing tests

--- scripts/debriefing.js
+++ scripts/debriefing.js
@@ -5,14 +5,14 @@ var sendMessage = function(msg) {
 };
 
 const debriefing = {
-    type: jsPsychHtmlKeyboardResponse,
+    type: jsPsychHtmlButtonResponse,
     stimulus: `<div class="normal-text">Vielen Dank für Ihre Teilnahme!</br>
     Wir möchten uns ganz herzlich für Ihre Mithilfe bedanken.</br>
     Im Folgenden werden wir Ihnen noch einige Hintergründe zu unserer Studie erläutern. Sie haben zu Beginn der Aufgabe eine Instruktion erhalten. Diese lautete entweder so viele Durchgänge wie möglich richtig zu machen oder so wenige Fehler wie möglich zu machen. Wir untersuchen, ob Unterschiede abhängig von der Instruktion auftreten. Dabei vergleichen wir jeweils die Reaktionszeiten nach einem Fehler. Wenn Sie Interesse an den Ergebnissen haben, schreiben Sie gerne eine Mail an:</br>
-    maja.krause01@stud.uni-heidelberg.de</br>
-    Sie können das Browser Fenster nun schließen.</div>`,
+    maja.krause01@stud.uni-heidelberg.de</br></div>`,
+    choices: "Weiter",
     data: {type: 'instructions'},
-    on_start: function() {
+    on_finish: function() {
       jsPsych.data.get().filter([{type: 'trial'}, {type: 'survey'}]).localSave('csv', experiment_file);
       console.log("Message sent");
 
