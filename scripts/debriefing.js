b53e7fc0ce65a6a305472289ba9d2b53da2a6f09
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-05-07 10:15:11 +0200

    Update debriefing.js

--- scripts/debriefing.js
+++ scripts/debriefing.js
@@ -6,9 +6,9 @@ var sendMessage = function(msg) {
 
 const debriefing = {
     type: jsPsychHtmlButtonResponse,
-    stimulus: `<div class="normal-text">Vielen Dank für Ihre Teilnahme!</br>
-    Wir möchten uns ganz herzlich für Ihre Mithilfe bedanken.</br>
-    Im Folgenden werden wir Ihnen noch einige Hintergründe zu unserer Studie erläutern. Sie haben zu Beginn der Aufgabe eine Instruktion erhalten. Diese lautete entweder so viele Durchgänge wie möglich richtig zu machen oder so wenige Fehler wie möglich zu machen. Wir untersuchen, ob Unterschiede abhängig von der Instruktion auftreten. Dabei vergleichen wir jeweils die Reaktionszeiten nach einem Fehler. Wenn Sie Interesse an den Ergebnissen haben, schreiben Sie gerne eine Mail an:</br>
+    stimulus: `<div class="normal-text">Vielen Dank für Deine Teilnahme!</br>
+    Wir möchten uns ganz herzlich für Deine Mithilfe bedanken.</br>
+    Im Folgenden werden wir dir noch einige Hintergründe zu unserer Studie erläutern. Du hast zu Beginn der Aufgabe eine Instruktion erhalten. Diese lautete entweder so viele Durchgänge wie möglich richtig zu machen oder so wenige Fehler wie möglich zu machen. Wir untersuchen, ob Unterschiede abhängig von der Instruktion auftreten. Dabei vergleichen wir jeweils die Reaktionszeiten nach einem Fehler. Wenn du Interesse an den Ergebnissen hast, schreib gerne eine Mail an:</br>
     maja.krause01@stud.uni-heidelberg.de</br></div>`,
     choices: ['Weiter'],
     data: {type: 'instructions'},
